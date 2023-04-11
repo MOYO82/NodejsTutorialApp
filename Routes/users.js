cce5bed510ff20b87a4ef63eebfd49111a6e4af0
MOYO82 <115661021+MOYO82@users.noreply.github.com>
2023-04-11 17:57:42 +0100

    Delete users.js

--- Routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router();
-
-router.get('/', function(request, response, next) {
-    res.send('responds')
-});
-
-
-module.exports = router
