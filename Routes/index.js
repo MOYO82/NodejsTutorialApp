d19580d0b6f1988766d30d1dd8730ffb0445f2ad
MOYO82 <115661021+MOYO82@users.noreply.github.com>
2023-04-11 17:57:24 +0100

    Delete index.js

--- Routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router();
-
-router.get('/', function(request, response, next) {
-    res.render('index', {title: 'express'})
-});
-
-
-module.exports = router
